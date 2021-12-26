24ca1507436524d3f1953758634e75d82cfcf334
Volodymyr Tsyhylyk <82731850+VolodymyrTsyhylyk@users.noreply.github.com>
2021-12-26 14:09:53 +0200

    HM the first stage

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+class App extends Component { 
+state = {
+  contacts: [],
+  name: ''
+}
+
+  inputId = nanoid();
+
+  render() { 
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <form>
+          <label>
+            Name
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+            />
+            </label>
+          </form>
+      </>
+    )
+  }
+}
+
+export default App;
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
